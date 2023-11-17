32a9e0de172824725f169075be9fe63c74520b2c
Alex <alex33376@hotmail.com>
2023-11-17 16:39:02 +0100

    Uppgift35.java

---
+++ src/Uppgifter/Uppgift35.java
@@ -0,0 +1,13 @@
+package Uppgifter;
+
+public class Uppgift35 {
+    public static void main(String[] args) {
+        //35.Skapa ett program som beräknar och skriver ut kvadraterna för talen 1 till 9.
+
+        for (int i = 1; i <= 9; i++){
+            int resultat = i*i;
+            System.out.println("Resultat:" + resultat);
+        }
+
+    }
+}
