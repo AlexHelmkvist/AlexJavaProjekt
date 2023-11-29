a6305e71938daa4f6580bbbd603b41068da82340
Alex <alex33376@hotmail.com>
2023-11-29 10:55:35 +0100

    Uppgift41.java

---
+++ src/Uppgifter/Uppgift41.java
@@ -0,0 +1,16 @@
+package Uppgifter;
+
+public class Uppgift41 {
+    public static void main(String[] args) {
+        //41.Skapa ett program som skriver ut summan av talen 1 till 50
+        int summa = 0;
+        for (int i = 1; i <= 50; i++){
+
+            summa = summa + i;
+
+
+            System.out.println("Summan av talen 1 till 50 är " + summa);
+        }
+
+    }
+}
