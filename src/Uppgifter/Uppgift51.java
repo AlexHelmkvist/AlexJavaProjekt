a6cb694fa22cbca92c94ee398061ac9f813fead3
Alex <alex33376@hotmail.com>
2023-12-07 13:26:27 +0100

    Uppgift51.java

--- src/Uppgifter/Uppgift51.java
+++ src/Uppgifter/Uppgift51.java
@@ -8,8 +8,19 @@ public class Uppgift51 {
         //lästs in.
         Scanner sc = new Scanner(System.in);
         System.out.println("Ange ett tal mellan 10 till 100:");
-        int n = sc.nextInt();
-        int antalTal7 = n / 7;
+
+        int antalTal7 = 0;
+
+        for (int i = 0;i<=100;i++){
+            int n = sc.nextInt();
+
+            if (n==7){
+                antalTal7++;
+
+            }
+
+        }
+
 
         System.out.println("Antal talet sju är:" + antalTal7);
 
