0e4b94e5103882fc4000b74c927ba47c518e0362
Alex <alex33376@hotmail.com>
2023-12-14 18:51:38 +0100

    Uppgift65.java

---
+++ src/Uppgifter/Uppgift65.java
@@ -0,0 +1,30 @@
+package Uppgifter;
+
+import java.util.Scanner;
+
+public class Uppgift65 {
+    public static void main(String[] args) {
+        //65.Indata består av 100 positiva tal. Skapa ett program för beräkning av skillnaden mellan det
+        //största talet och det näst största talet.
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ange 10 positiva tal.");
+
+        int big = Integer.MIN_VALUE;
+
+        for (int i = 0; i < 10; i++) {
+            int num = sc.nextInt();
+
+            if (num > big) {
+                big = num;
+
+            }
+        }
+        int nextBig = big +1;
+        int skillnaden = nextBig - big;
+
+        System.out.println("Störst tal: " + big + "\n" + "Nästa störst tal: " + nextBig + "\n" + "Skillnaden mellan största tal och näst tal: " + skillnaden);
+
+    }
+
+}
