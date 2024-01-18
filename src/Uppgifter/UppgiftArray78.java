ec9839e1f683d97a17d072961e3dc5ac977444c7
Alex <alex33376@hotmail.com>
2024-01-18 10:14:21 +0100

    UppgiftArray78.java

---
+++ src/Uppgifter/UppgiftArray78.java
@@ -0,0 +1,30 @@
+package Uppgifter;
+
+import java.util.Scanner;
+
+public class UppgiftArray78 {
+    public static void main(String[] args) {
+        //78.Skapa ett program som läser in siffrorna i ett bråk (täljare och nämnare) till en array med
+        //två platser. Eftersom det är otillåtet att ha 0 i nämnaren ska ett felmeddelande skrivas ut
+        //om så är fallet.
+
+        Scanner sc = new Scanner(System.in);//Skapar enn scanner för att läsa in information.
+        int[] arr = new int[2];           //Skapar array med två platser.
+
+        System.out.println("Ange täljare: ");//Ber användare att ange täljare.
+        arr[0] = sc.nextInt();              //Läser in information.
+
+        System.out.println("Ange nämnare:");//Ber användare att ange nämnare.
+        arr[1] = sc.nextInt();          //Läser in information.
+
+        if (arr[1] == 0) {
+
+            System.out.println("Det är otillåtet att ha 0 i nämnaren ");
+        } else {
+            System.out.println("Bråket är: " + arr[0] + "/" + arr[1]);
+
+        }
+
+    }
+
+}
