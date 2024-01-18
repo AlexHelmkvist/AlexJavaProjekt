da0d83c234bd7f712d4026fb8f958423bb1da5ae
Alex <alex33376@hotmail.com>
2024-01-18 10:29:42 +0100

    UppgiftArray79.java

--- src/Uppgifter/UppgiftArray79.java
+++ src/Uppgifter/UppgiftArray79.java
@@ -15,10 +15,10 @@ public class UppgiftArray79 {
         arr[0] = sc.nextInt();              //Läser in information.
 
         System.out.println("Ange nämnare:");//Ber användare att ange nämnare.
-        arr[1] = sc.nextInt();          //Läser in information.
+        arr[1] = sc.nextInt();              //Läser in information.
 
-        if (arr[1] <= 0) {
-            arr[1] = arr[1] * -1;
+        if (arr[1] <= 0) {                  //Om nämnare mindre eller lika med 0.
+            arr[1] = arr[1] * -1;           //Då nämnaren multipliceras med -1 för att få positiv värde.
             System.out.println("Bråket är: " + arr[0] + "/" + arr[1]);
 
         }
