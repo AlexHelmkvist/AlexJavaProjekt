e2d8217dd4187dbc280bfc50a74b2a96edfcaf0e
Alex <alex33376@hotmail.com>
2024-02-11 16:15:04 +0100

    BookSwitch.java

--- src/Uppgifter/BookSwitch.java
+++ src/Uppgifter/BookSwitch.java
@@ -1,7 +1,7 @@
 package Uppgifter;
 
-public class Book {
-        //Tern operator
+public class BookSwitch {
+    //Tern operator
     public static void main(String[] args) {
 
         for (int number = -5;number <= 5;number++){
@@ -10,30 +10,37 @@ public class Book {
             System.out.println(" Värde = " + absval);
         }
 
-        for (int x = 1; x <= 13;x++){
-            switch (x){
-                case 1: case 2: case 12:
-                    System.out.println(x + ": vinter" );
+        for (int x = 1; x <= 13; x++) {
+            switch (x) {
+                case 1:
+                case 2:
+                case 12:
+                    System.out.println(x + ": vinter");
                     break;
-                case 3: case 4: case 5:
+                case 3:
+                case 4:
+                case 5:
                     System.out.println(x + ":vår");
                     break;
-                case 6: case 7: case 8:
+                case 6:
+                case 7:
+                case 8:
                     System.out.println(x + ":sommar");
                     break;
-                case 9: case 10: case 11:
+                case 9:
+                case 10:
+                case 11:
                     System.out.println(x + ":höst");
                     break;
                 default:
                     System.out.println(x + ":det finns inga sådana månaden");
 
 
-
             }
 
         }
         for (int x = 100;x > 0;x -=5){ //<---Minskar med 5 varje gång
-            System.out.println("x = " + x);
+         System.out.println("x = " + x);
         }
 
 
